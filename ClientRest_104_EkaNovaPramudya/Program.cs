d4857d8494bbe7f3a91707258299d9744f50bfc5
Eka Nova Pramudya <78529450+pramek008@users.noreply.github.com>
2021-12-31 22:35:13 +0700

    Add project files.

---
+++ ClientRest_104_EkaNovaPramudya/Program.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientRest_104_EkaNovaPramudya
+{
+    [DataContract]
+    public class Mahasiswa
+    {
+        private string _nama, _nim, _prodi, _angkatan;
+        [DataMember]
+        public string nama
+        {
+            get { return _nama; }
+            set { _nama = value; }
+        }
+        [DataMember]
+        public string nim
+        {
+            get { return _nim; }
+            set { _nim = value; }
+        }
+        [DataMember]
+        public string prodi
+        {
+            get { return _prodi; }
+            set { _prodi = value; }
+        }
+        [DataMember]
+        public string angkatan
+        {
+            get { return _angkatan; }
+            set { _angkatan = value; }
+        }
+    }
+    
+    class ClassData
+    {
+        public void getData()
+        {
+            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+
+            foreach (var mhs in data)
+            {
+                Console.WriteLine("Nama: " + mhs.nama);
+                Console.WriteLine("NIM: " + mhs.nim);
+                Console.WriteLine("Prodi: " + mhs.prodi);
+                Console.WriteLine("Angkatan: " + mhs.angkatan);
+            }
+            Console.ReadLine();
+        }
+    }
+    class Program
+    {        
+        string baseUrl = "http://localhost:1907/";
+
+        void BuatMahasiswa()
+        {
+            Mahasiswa mhs = new Mahasiswa();
+            Console.WriteLine("Masukan Nama: ");
+            mhs.nama = Console.ReadLine();
+            Console.WriteLine("Masukan NIM: ");
+            mhs.nim = Console.ReadLine();
+            Console.WriteLine("Masukan Prodi: ");
+            mhs.prodi = Console.ReadLine();
+            Console.WriteLine("Masukan Angkatan: ");
+            mhs.angkatan = Console.ReadLine();
+
+            var data = JsonConvert.SerializeObject(mhs);
+            var postdata = new WebClient();
+            postdata.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+            string response = postdata.UploadString(baseUrl + "Mahasiswa", data);
+            Console.WriteLine(response);
+        }        
+
+        static void Main(string[] args)
+        {
+            ClassData cl = new ClassData();
+            cl.getData();
+            Program app = new Program();
+            app.BuatMahasiswa();
+            Console.ReadLine();
+        }
+    }
+}
